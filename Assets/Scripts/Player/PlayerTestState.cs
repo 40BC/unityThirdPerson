bf72aa7decac85a1cc342d9eadb63a96ed67a5cc
Brandon <40BC@users.noreply.github.com>
2022-06-14 16:22:04 -0500

    Implemented Input Events

--- Assets/Scripts/Player/PlayerTestState.cs
+++ Assets/Scripts/Player/PlayerTestState.cs
@@ -1,28 +1,29 @@
+using System;
 using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timeToSwitch = Mathf.Infinity;
+    private float timeToSwitch = 0f;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
-        Debug.Log("Enter");
-        timeToSwitch = 10f;
+        stateMachine.InputReader.JumpEvent += OnJump; 
     }
 
     public override void Tick(float deltaTime)
     {
-        timeToSwitch -= deltaTime;
-        Debug.Log("Time to Switch " + timeToSwitch);
-
-        if (timeToSwitch <= 0f) {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
+        timeToSwitch += deltaTime;
+        Debug.Log(timeToSwitch);
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
